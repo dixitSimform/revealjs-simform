09ac35e0677b9526cd0a06d6d96fe01271ac26fa
dixit_pandya <dixit.p@simformsolutions.com>
2023-05-25 16:11:07 +0530

    Add components theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
       transparent: "transparent",
       white: "#ffffff",
       black: "000000",
+      zinc: {
+        100: "#f4f4f5",
+      },
       slate: {
         100: "#f0f9ff",
         200: "#d8d8d8",
         600: "#3d3d3d",
       },
       rose: {
+        400: "#fb7185",
         500: "#ef5366",
         600: "#ff4d63",
       },
